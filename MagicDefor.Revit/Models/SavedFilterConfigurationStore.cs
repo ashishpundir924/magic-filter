a869ed10ee2fc6892e5246b5856a569cae4f7028
Ashish Pundir <96501368+ashishpundir924@users.noreply.github.com>
2026-04-18 14:56:07 +0200

    Add saved configuration store

---
+++ MagicDefor.Revit/Models/SavedFilterConfigurationStore.cs
@@ -0,0 +1,6 @@
+namespace MagicDefor.Revit.Models;
+
+public sealed class SavedFilterConfigurationStore
+{
+    public List<SavedFilterConfiguration> Configurations { get; set; } = new List<SavedFilterConfiguration>();
+}
